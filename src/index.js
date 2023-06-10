a4b5a4318789d37e7dd87923faf4fc41ae9c423b
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-06-10 08:25:01 +0500

    configlarni sozla

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const config = require('./shared/config')
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server 3000-portda ishlayapti');
+console.log(config)
+
+app.listen(config.port, () => {
+  console.log(`Server ${config.port}-portda ishlayapti`);
 });
