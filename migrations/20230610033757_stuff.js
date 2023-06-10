1f2fd9f658a182f820b96071c50891b55ab19763
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-06-10 08:50:22 +0500

    stuff uchun migration qo'sh

---
+++ migrations/20230610033757_stuff.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('stuff', (table) => {
+    table.increments('id').primary();
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.enum('role', ['admin', 'teacher', 'assistent_teacher', 'super_admin']);
+    table.string('username', 10);
+    table.string('password', 300);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('stuff');
+};
