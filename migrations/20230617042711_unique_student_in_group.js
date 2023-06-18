cf4e9c3c81351775949c6ef347eb9cd06de547e2
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-06-18 16:41:37 +0500

    bitta guruhda student unikal

---
+++ migrations/20230617042711_unique_student_in_group.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('groups_students', (table) => {
+    table.unique(['student_id']);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('groups_students', (table) => {
+    table.dropUnique(['student_id', 'group_id']);
+  });
+};
